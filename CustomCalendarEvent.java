602f61d6b8bdf05b39b7d233e0bffbaa800fd6bf
Frosty <108729205+LordFrostwolf@users.noreply.github.com>
2022-11-24 09:46:42 +0100

    Added internal Try-Catchs
    Added internal Try-Catchs to help with parsing time and date

--- CustomCalendarEvent.java
+++ CustomCalendarEvent.java
@@ -9,29 +9,49 @@ public class CustomCalendarEvent{
     private LocalDate date;
     private DayOfWeek day;
     
+    private void parseLocalTime(String time){
+        try {
+            this.time = LocalTime.parse(time);
+        } catch (Exception e) {
+            System.out.println("Error while parsing time.");
+            this.time = LocalTime.now();
+        }
+    }
+
+    private void parseLocalDate(String date){
+        try {
+            this.date = LocalDate.parse(date);
+        } catch (Exception e) {
+            System.out.println("Error while parsing date.");
+            this.date = LocalDate.now();
+        }
+    }
+
     /**
      * Creates a new calendar event with the given name, time and date.
      * <br> It also automatically stores the day of the week according to the date.
+     * <br> If parsing the time or date creates an exception, sets these values to the current values of the system clock.
      * @param name the name of the event
      * @param time the time of the event
      * @param date the date of the event
      */
-    public CustomCalendarEvent(String name, LocalTime time, LocalDate date){
+    public CustomCalendarEvent(String name, String time, String date){
         this.name = name;
-        this.time = time;
-        this.date = date;
-        this.day = date.getDayOfWeek();
+        parseLocalTime(time);
+        parseLocalDate(date);
+        this.day = this.date.getDayOfWeek();
     }
 
     /**
      * Creates a new recurring calendar event with the given name, time and day of the week.
+     * <br> If parsing the time creates an exception, sets this value to the current value of the system clock.
      * @param name the name of the event
      * @param time the time of the event
      * @param day the day of week of the event
      */
-    public CustomCalendarEvent(String name, LocalTime time, DayOfWeek day){
+    public CustomCalendarEvent(String name, String time, DayOfWeek day){
         this.name = name;
-        this.time = time;
+        parseLocalTime(time);
         this.day = day;
     }
 
@@ -49,13 +69,14 @@ public class CustomCalendarEvent{
     /**
      * Creates a new calendar event with the given name and date.
      * <br> This event is interpreted as a whole day event.
+     * <br> If parsing the date creates an exception, sets this value to the current value of the system clock.
      * @param name the name of the event
      * @param date the day of the week of the event
      */
-    public CustomCalendarEvent(String name, LocalDate date){
+    public CustomCalendarEvent(String name, String date){
         this.name = name;
-        this.date = date;
-        this.day = date.getDayOfWeek();
+        parseLocalDate(date);;
+        this.day = this.date.getDayOfWeek();
     }
 
     /**
@@ -113,20 +134,22 @@ public class CustomCalendarEvent{
     /**
      * Sets the time of the event.
      * <br> To create a recurring event, set the time to {@code null}.
+     * <br> If parsing the time creates an exception, sets this value to the current value of the system clock.
      * @param time the time of the event
      */
-    public void setTime(LocalTime time){
-        this.time = time;
+    public void setTime(String time){
+        parseLocalTime(time);;
     }
 
     /**
      * Sets the date of the event.
      * <br> If the event was a recurring event, it now is a not recurring event.
+     * <br> If parsing the date creates an exception, sets this value to the current value of the system clock.
      * @param date the date of the event
      */
-    public void setDate(LocalDate date){
-        this.date = date;
-        day = date.getDayOfWeek();
+    public void setDate(String date){
+        parseLocalDate(date);;
+        day = this.date.getDayOfWeek();
     }
 
     /**
