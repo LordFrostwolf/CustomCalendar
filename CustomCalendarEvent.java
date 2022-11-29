d4c4cc6dde3475c49cad4d90460582fd53b14e1e
Frosty <108729205+LordFrostwolf@users.noreply.github.com>
2022-11-29 16:54:03 +0100

    Add endtime to CustomCalendar

--- CustomCalendarEvent.java
+++ CustomCalendarEvent.java
@@ -8,6 +8,7 @@ public class CustomCalendarEvent{
     private LocalTime time;
     private LocalDate date;
     private DayOfWeek day;
+    private LocalTime endTime;
     
     private void parseLocalTime(String time){
         try {
@@ -15,6 +16,15 @@ public class CustomCalendarEvent{
         } catch (Exception e) {
             System.out.println("Error while parsing time.");
             this.time = LocalTime.now();
+        }
+    }
+
+    private void parseLocalEndTime(String time){
+        try {
+            this.endTime = LocalTime.parse(time);
+        } catch (Exception e) {
+            System.out.println("Error while parsing time.");
+            this.endTime = LocalTime.now();
         }
     }
 
@@ -28,30 +38,34 @@ public class CustomCalendarEvent{
     }
 
     /**
-     * Creates a new calendar event with the given name, time and date.
+     * Creates a new calendar event with the given name, time, endtime and date.
      * <br> It also automatically stores the day of the week according to the date.
      * <br> If parsing the time or date creates an exception, sets these values to the current values of the system clock.
      * @param name the name of the event
      * @param time the time of the event
+     * @param endTime the endtime of the event
      * @param date the date of the event
      */
-    public CustomCalendarEvent(String name, String time, String date){
+    public CustomCalendarEvent(String name, String time, String endTime, String date){
         this.name = name;
         parseLocalTime(time);
+        parseLocalEndTime(endTime);
         parseLocalDate(date);
         this.day = this.date.getDayOfWeek();
     }
 
     /**
-     * Creates a new recurring calendar event with the given name, time and day of the week.
+     * Creates a new recurring calendar event with the given name, time, endtime and day of the week.
      * <br> If parsing the time creates an exception, sets this value to the current value of the system clock.
      * @param name the name of the event
      * @param time the time of the event
+     * @param endTime the endtime of the event
      * @param day the day of week of the event
      */
-    public CustomCalendarEvent(String name, String time, DayOfWeek day){
+    public CustomCalendarEvent(String name, String time, String endTime, DayOfWeek day){
         this.name = name;
         parseLocalTime(time);
+        parseLocalEndTime(endTime);
         this.day = day;
     }
 
@@ -89,7 +103,7 @@ public class CustomCalendarEvent{
 
     /**
      * Returns the time of the event.
-     * <br> If the event is a recurring event, returns 00:00:00.
+     * <br> If the event is a whole day event, returns 00:00:00.
      * @return the time of the event
      */
     public LocalTime getTime(){
@@ -97,6 +111,18 @@ public class CustomCalendarEvent{
             return LocalTime.MIDNIGHT;
         }
         return time;
+    }
+
+    /**
+     * Returns the endtime of the event.
+     * <br> If the event is a whole day event, returns 00:00:00.
+     * @return the endtime of the event.
+     */
+    public LocalTime getEndTime(){
+        if(endTime == null){
+            return LocalTime.MIDNIGHT;
+        }
+        return endTime;
     }
 
     /**
@@ -133,12 +159,27 @@ public class CustomCalendarEvent{
 
     /**
      * Sets the time of the event.
-     * <br> To create a recurring event, set the time to {@code null}.
+     * <br> To create a whole day event, set the time to {@code null}.
      * <br> If parsing the time creates an exception, sets this value to the current value of the system clock.
      * @param time the time of the event
      */
     public void setTime(String time){
-        parseLocalTime(time);;
+        parseLocalTime(time);
+        if(time == null){
+            endTime = null;
+        }
+    }
+
+    /**
+     * Sets the endtime of the event.
+     * <br> To create a whole day event, set the time with {@code setTime} to {@code null}.
+     * <br> If parsing the time creates an exception, sets this value to the current value of the system clock.
+     * @param endTime the endtime of the event
+     */
+    public void setEndTime(String endTime){
+        if(endTime != null){
+            parseLocalEndTime(endTime);
+        }
     }
 
     /**
